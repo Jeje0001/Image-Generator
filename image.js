11c7267bff56c4e44c5e82b619817680424125ce
jejea <157323284+Jeje0001@users.noreply.github.com>
2025-02-03 06:05:03 -0500

    changes

---
+++ image.js
@@ -0,0 +1,6 @@
+const searchform=document.getElementById("search-form")
+const searchbox=document.getElementById("search-box")
+const searchresult=document.getElementById("search-result")
+const showmorebutton=document.getElementById("show-more")
+
+// continue at 17:52
