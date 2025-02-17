09c1e8c0d164550ad27deb4e96e8288fbd25b45d
jejea <157323284+Jeje0001@users.noreply.github.com>
2025-02-17 07:10:51 -0500

    changes made

--- image.js
+++ image.js
@@ -3,18 +3,29 @@ const searchbox=document.getElementById("search-box")
 const searchresult=document.getElementById("search-result")
 const showmorebutton=document.getElementById("show-more")
 
-// continue at 22:04
+// continue at 30:30
 let keyword="";
 let page=1;
 let clientid="wbe-R6JpY1P6dz5RBU-WTYJ3NzA7RXiE3ybR9IO8YZA"
 async function searchimages() {
     keyword=searchbox.value;
-    const url=`https://api.unsplash.com/search/collections?page=${page}&query=${keyword}&client_id=${clientid}`;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${clientid}&per_page=12`;
     
     const response= await fetch (url);
     const data= await response.json();
 
-    console.log(data);
+    const result= data.results;
+    console.log(result)
+    result.map((result)=>{
+        const image=document.createElement("img")
+        image.src=result.urls.small
+        const imagelik=document.createElement("a")
+        imagelik.href=result.links.html
+        imagelik.target="_blank";
+
+        imagelik.appendChild(image);
+        searchresult.appendChild(imagelik);
+    })
     
     
 }
